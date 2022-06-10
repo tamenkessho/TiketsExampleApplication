caa1ab78e1e45a08cf86b0cbfd52855718762c53
sea <artur.semenenko@nag.ch>
2022-06-10 13:49:46 +0200

    GUI is finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NAGTicketsApplication';
+  addingMenuStatus = false;
+  manageDataStatus = false;
+  text1: string = "Adding menu";
+  text2: string = "Manage data";
+
+  addingMenuOpen() {
+    this.addingMenuStatus = !this.addingMenuStatus
+    this.manageDataStatus = false
+    this.text2 = "Manage data"
+    if(this.addingMenuStatus){
+      this.text1 = "Close menu"
+    } else {this.text1 = "Adding menu"}
+
+  }
+  manageDataOpen() {
+    this.manageDataStatus = !this.manageDataStatus
+    this.addingMenuStatus = false
+    this.text1 = "Adding menu"
+    if(this.manageDataStatus){
+      this.text2 = "Close menu"
+    }else{this.text2 = "Manage data"}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatSliderModule} from '@angular/material/slider';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {AddMenuComponent} from "./add.menu/add.menu.component";
+import { ManageDataComponent } from './manage-data/manage-data.component';
+import { TicketViewComponent } from './ticket-view/ticket-view.component';
+import { TicketComponent } from './ticket-view/ticket/ticket.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMenuComponent,
+    ManageDataComponent,
+    TicketViewComponent,
+    TicketComponent
   ],
   imports: [
-    BrowserModule
+    MatSliderModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
